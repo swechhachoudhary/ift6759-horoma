67342be697e6f209878bb11fe612370dd52ac0bb
user name <user11@helios1.helios>
2019-04-03 11:57:27 -0400

    Update config file for DAMIC + clean and refactorize train_damic and damic_utils

--- train_damic.py
+++ train_damic.py
@@ -9,43 +9,48 @@ from utils.constants import Constants
 import torch
 from data.dataset import LocalHoromaDataset
 
-def main(datapath, encoding_model, clustering_model, batch_size, n_epochs, lr, flattened, device, overlapped,
-         experiment, train_encoder, train_subset=None, path_to_model=None):
+def main(datapath, configuration, config_key):
     """
     :param datapath: path to the directory containing the samples
-    :param encoding_model: which encoding model to use for the pre-training of DAMIC
-    :param clustering_model: clustering model used for the pre-training of DAMIC
-    :param batch_size: batch size
-    :param n_epochs: number of epochs
-    :param lr: learning rate
-    :param flattened: If True return the images in a flatten format.
-    :param device: use CUDA device if available else CPU .
-    :param overlapped: boolean, if True use the overlapped pixel patches.
-    :param experiment: track experiment
-    :param train_encoder: if True, an auto-encoder is trained on the unlabeled data for DAMIC pre-training
-    :param train_subset: whether of not we use a subset for the dataset specified in datapath
-    :param path_to_model: if train_encoder is False, then we load an autoencoder from there
-    """   
+    :param configuration: dictionnary containing all the keys/values part of the config_key json file
+    :param config_key: key of the configuration we have to load from the configuration file (Ex: DAMIC)
+    """ 
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    seed = configuration['seed']
+    _set_torch_seed(seed)
+
+    experiment = _set_comet_experiment(configuration, config_key)
+    
     damic_model = DAMICClustering(17).to(device)
-    damic_model, numpy_unla_train, numpy_unla_target_pred_by_cluster = execute_damic_pre_training(datapath, damic_model, encoding_model, 
-                                                                                                  train_encoder, clustering_model,
-                                                                                                  batch_size, n_epochs, lr, flattened,
-                                                                                                  device, overlapped, experiment,
-                                                                                                  train_subset, path_to_model)
+
+    ae_pretrain_config = configuration['autoencoder_pretrain']
+    conv_net_pretrain_config = configuration['damic_conv_net_pretrain']
+    damic_autoencoders_pretrain_config = configuration['damic_autoencoders_pretrain']
+    train_subset = configuration['train_subset']
+    overlapped = configuration['overlapped']
+    damic_model, numpy_unla_train, numpy_unla_target_pred_by_cluster = execute_damic_pre_training(datapath, damic_model, train_subset,
+                                                                                                  overlapped, ae_pretrain_config,
+                                                                                                  conv_net_pretrain_config,
+                                                                                                  damic_autoencoders_pretrain_config,
+                                                                                                  experiment, seed)
 
     print("== Start DAMIC training ...!")
+    damic_train_config = configuration['damic_train']
     pretrain_dataset_with_label = LocalHoromaDataset(numpy_unla_train, numpy_unla_target_pred_by_cluster)
     clust_network_params = list(damic_model.clustering_network.parameters()) + list(damic_model.output_layer_conv_net.parameters())
     autoencoders_params = list()
     for i in range(17):
         autoencoders_params = autoencoders_params + list(damic_model.autoencoders[i].parameters())
     damic_parameters = clust_network_params + autoencoders_params
+    lr = damic_train_config["lr"]
+    n_epochs = damic_train_config["n_epochs"]
     optimizer = torch.optim.Adam(damic_parameters, lr=lr)
     damic_model = train_network(damic_model,
                                pretrain_dataset_with_label,
                                None,
                                optimizer,
-                               1,
+                               n_epochs,
                                device,
                                experiment,
                                train_classifier=False,
@@ -53,65 +58,34 @@ def main(datapath, encoding_model, clustering_model, batch_size, n_epochs, lr, f
 
     print("== DAMIC training done!")
     torch.save(damic_model, Constants.PATH_TO_MODEL + "DAMIC_MODEL" + str(experiment.get_key()) + '.pth')
+    
+def _set_torch_seed(seed):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    
+def _set_comet_experiment(configuration, config_key):
+    experiment = OfflineExperiment(project_name='general',
+                                   workspace='benjaminbenoit',
+                                   offline_directory="experiments")
+    experiment.set_name(config_key)
+    experiment.log_parameters(configuration)
+    return experiment
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--datapath", type=str, default=Constants.DATAPATH,
                         help="Path to dataset folder")
-    parser.add_argument("--encoder_path", type=str, default=None)
-    parser.add_argument("--config", type=str, default="CONV_AE", help="To select configuration from config.json")
+    parser.add_argument("--config", type=str, default="DAMIC", help="To select configuration from config.json")
     args = parser.parse_args()
     config_key = args.config
     datapath = args.datapath
-    path_to_model = args.encoder_path
 
     with open(Constants.CONFIG_PATH, 'r') as f:
         configuration = json.load(f)[config_key]
 
-    # Parse configuration file
-    encoding_model = configuration['enc_model']
-    batch_size = configuration['batch_size']
-    seed = configuration['seed']
-    n_epochs = configuration['n_epochs']
-    lr = configuration['lr']
-    n_clusters = configuration['n_clusters']
-    train_subset = configuration['train_subset']
-    overlapped = configuration['overlapped']
-    # If true, we train an autoencoder and do a kmean clustering to pretrain the clustering network
-    train_encoder = configuration['train_encoder']
-    latent_dim = configuration['latent_dim']
-    flattened = False  # Default
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-    # Set all seeds for full reproducibility
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
-
-    # Set up Comet Experiment tracking
-    experiment = OfflineExperiment(project_name='general',
-                                   workspace='benjaminbenoit',
-                                   offline_directory="experiments")
-    experiment.set_name(
-        name=args.config + "_dim={}_cluster={}_overlapped={}".format(latent_dim, n_clusters, overlapped))
-    experiment.log_parameters(configuration)
-
-    # Initialize necessary objects
-    clustering_model = KMeansClustering(n_clusters, seed)
-
-    if encoding_model == "cvae":
-        encoding_model = CVAE(latent_dim=latent_dim).to(device)
-        flattened = False
-    elif encoding_model == "convae":
-        encoding_model = ConvAE(latent_dim=latent_dim).to(device)
-        flattened = False
-    else:
-        print('No encoding model specified. Using CVAE.')
-        encoding_model = CVAE(seed)
-
     # Initiate experiment
-    main(datapath, encoding_model, clustering_model, batch_size, n_epochs, lr, flattened, device, overlapped,
-         experiment, train_encoder, train_subset, path_to_model)
+    main(datapath, configuration, config_key)
--- utils/damic_utils.py
+++ utils/damic_utils.py
@@ -34,15 +34,29 @@ def get_class_prediction(encoding_model, clustering_model, encoded_unlabeled_tra
     return numpy_unla_train, numpy_unla_target_pred_by_cluster
 
 
-def execute_damic_pre_training(datapath, damic_model, encoding_model, train_encoder, clustering_model,
-                                batch_size, n_epochs, lr, flattened, device, overlapped,
-                                 experiment, train_subset, path_to_model):
+def _get_encoding_model(encoding_model_name, latent_dim, device, seed):
+    if encoding_model_name == "cvae":
+        encoding_model = CVAE(latent_dim=latent_dim).to(device)
+    elif encoding_model_name == "convae":
+        encoding_model = ConvAE(latent_dim=latent_dim).to(device)
+    else:
+        print('No encoding model specified. Using CVAE.')
+        encoding_model = CVAE(seed)
+    return encoding_model
+        
+    
+def _get_clustering_model(n_clusters, seed):
+    return KMeansClustering(n_clusters, seed)
+
+
+def execute_damic_pre_training(datapath, damic_model, train_subset, overlapped, ae_pretrain_config, conv_net_pretrain_config,
+                               damic_autoencoders_pretrain_config, experiment, seed):
     """
     See 'Deep clustering based on a mixture of autoencoders' paper
     DAMIC needs a pre-training to initialize both weights for the autoencoders and the
     convolutional clustering network.
     At first we train one autoencoder (encoding_model) on all the unlabeled data available
-    We apply one clustering model (clustering_model) to label those unlabeled data
+    Then, we apply one clustering model (clustering_model) to label those unlabeled data
     After that, we train each autoencoder of DAMIC (1 encoder for each cluster/targets) only
     on the data of the same class. Ex: autoencoder at index 0 will be only trained on data
     labeled by the clustering model as being of class 0.
@@ -51,29 +65,42 @@ def execute_damic_pre_training(datapath, damic_model, encoding_model, train_enco
     
     :param datapath: datapath for the data to do the pre training on
     :param damic_model: the model we want to do the pre-training on
-    :param encoding_model: which encoding model to use for the pre-training of DAMIC
-    :param train_autoencoder: if True, the auto encoder is trained from scratch
-    :param clustering_model: clustering model used for the pre-training of DAMIC
+    :param train_subset: how many data from the dataset we should do the training on
+    :param overlapped: if True, we use the overlapped datasets
+    :param ae_pretrain_config: configuration dictionary for the auto-encoder used during pre-training
+    :param conv_net_pretrain_config: configuration dictionary for the convolutional clustering network for pre-tranining
+    :param damic_autoencoders_pretrain_config: configuration dictionary for all the autoencoders part of DAMIC for pre-training
+    :param experiment: comet-ml experiment to save training results
+    :seed: seed for reproducible results
     """
     
     unlabeled_train, labeled_train, labeled_valid = load_original_horoma_datasets(datapath, train_subset=train_subset,
-                                   flattened=flattened, overlapped=overlapped)
+                                                                                  overlapped=overlapped)
+    
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    latent_dim = ae_pretrain_config["latent_dim"]
+    encoding_model = _get_encoding_model(ae_pretrain_config["enc_model"], latent_dim, device, seed)
+    n_clusters = ae_pretrain_config["n_clusters"]
+    clustering_model = _get_clustering_model(n_clusters, seed)
 
     print("Shape of unlabeled training set: ", unlabeled_train.data.shape)
     print("Shape of labeled training set: ", labeled_train.data.shape)
     print("Shape of labeled valid set: ", labeled_valid.data.shape)
     
     print("== Start DAMIC pre-training ...")
-    # Train a single autoencoder for the entire dataset
-    if train_encoder:
-        # Train and apply encoding model
+    batch_size = ae_pretrain_config['batch_size']
+    n_epochs = ae_pretrain_config['n_epochs']
+    lr = ae_pretrain_config['lr']
+    if ae_pretrain_config['train_encoder']:
         print("Start training of pre-train auto-encoder...")
-        encoded_unlabeled_train, encoding_model = encoding_model.fit(data=unlabeled_train, batch_size=batch_size, n_epochs=n_epochs,
-                                                       lr=lr, device=device, experiment=experiment)
+        encoded_unlabeled_train, encoding_model = encoding_model.fit(data=unlabeled_train, batch_size=batch_size, n_epochs=n_epochs, lr=lr,
+                                                                     device=device, experiment=experiment)
         print("Done")
     else:
         # Load encoding model and apply encoding
         print("Load pre-train auto-encoder...")
+        path_to_model = ae_pretrain_config["encoder_path"]
         encoding_model.load_state_dict(torch.load(path_to_model)["model"])
         print("Done")
         print("Start encoding of unlabeled dataset...")
@@ -89,11 +116,17 @@ def execute_damic_pre_training(datapath, damic_model, encoding_model, train_enco
     print("Start pre-training of clustering convolutional model...")
     pretrain_dataset_with_label = LocalHoromaDataset(numpy_unla_train, numpy_unla_target_pred_by_cluster)
     clust_network_params = list(damic_model.clustering_network.parameters()) + list(damic_model.output_layer_conv_net.parameters())
+    lr = conv_net_pretrain_config["lr"]
+    n_epoch = conv_net_pretrain_config["n_epochs"]
     optimizer = torch.optim.Adam(clust_network_params, lr=lr)
-    damic_model = train_network(damic_model, pretrain_dataset_with_label, None, optimizer, 1, device, experiment, train_classifier=True)
+    damic_model = train_network(damic_model, pretrain_dataset_with_label, None, optimizer, n_epoch, device, experiment,
+                                train_classifier=True)
     print("Done")
 
     # Train each auto encoder of each cluster on his own data class
+    lr = damic_autoencoders_pretrain_config["lr"]
+    n_epochs = damic_autoencoders_pretrain_config["n_epochs"]
+    batch_size = damic_autoencoders_pretrain_config["batch_size"]
     for i in range(17):
         print("Start pre-training of auto encoder for cluster ...")
         indexes_of_class_i = np.where(np.isin(numpy_unla_target_pred_by_cluster,[i]))
